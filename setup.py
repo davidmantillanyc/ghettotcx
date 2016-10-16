6abc05e6674191ec662ac10e2bd40c5ac6a884ad
David Mantilla <davidmantillanyc@gmail.com>
2016-10-16 02:56:43 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-        version=0.1,
+        version='0.2.dev0',
         name="ghettotcx",
         package_dir={'ghettotcx': 'ghettotcx'},
         packages=find_packages(),
